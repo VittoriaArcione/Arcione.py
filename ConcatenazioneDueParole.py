058c8561d191eb2de4f0bd410515a3ed3bd61208
VittoriaArcione <73583881+VittoriaArcione@users.noreply.github.com>
2020-12-30 17:36:15 +0100

    Create ConcatenazioneDueParole.py

---
+++ ConcatenazioneDueParole.py
@@ -0,0 +1,4 @@
+prima_parola = input("Inserisci una parola: ")
+seconda_parola = input("Inserisci una parola: ")
+terza_parola = (prima_parola + " " + seconda_parola)
+print("La concatenazione di " + prima_parola + " e " + seconda_parola + " è " + terza_parola)
