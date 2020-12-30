e1e644c436ac0c196b0234119dc04fad144164e2
VittoriaArcione <73583881+VittoriaArcione@users.noreply.github.com>
2020-12-30 17:31:05 +0100

    Create GraficoVaiolo.py

---
+++ GraficoVaiolo.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1887, 1892, 1897, 1902, 1907, 1912, 1917, 1922])
+ypoints = np.array([16000, 2000, 4000, 6000, 1000, 5000, 0, 16000])
+
+plt.plot(xpoints, ypoints)
+plt.show()
