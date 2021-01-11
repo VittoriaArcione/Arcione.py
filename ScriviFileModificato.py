c679603b6f2fab49354e0adcd5ea5e565023f59f
VittoriaArcione <73583881+VittoriaArcione@users.noreply.github.com>
2021-01-11 10:40:55 +0100

    changes

---
+++ ScriviFileModificato.py
@@ -0,0 +1,23 @@
+from random import randint
+
+f = open("dati.txt", 'w')
+
+dati = ""
+
+for riga in range(100):
+
+    
+    for elemento in range(1):
+
+        
+        dati = dati + str(randint(1,100)) + "," + str(randint(1,100))
+    
+    
+    dati = dati + "\n"
+
+
+print(dati)
+
+f.write(dati)
+
+f.close()
