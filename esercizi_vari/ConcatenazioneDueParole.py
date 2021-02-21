812b06028ce49de00efaaf5585b62309d93ce378
VittoriaArcione <73583881+VittoriaArcione@users.noreply.github.com>
2021-02-21 07:46:24 +0100

    rename

--- esercizi_vari/ConcatenazioneDueParole.py
+++
@@ -1,4 +0,0 @@
-prima_parola = input("Inserisci una parola: ")
-seconda_parola = input("Inserisci una parola: ")
-terza_parola = (prima_parola + " " + seconda_parola)
-print("La concatenazione di " + prima_parola + " e " + seconda_parola + " è " + terza_parola)
