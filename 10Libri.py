3aa9856b874d8966f36013b82a74f6a5407a1e79
VittoriaArcione <73583881+VittoriaArcione@users.noreply.github.com>
2021-03-02 21:57:03 +0100

    esercizio

---
+++ 10Libri.py
@@ -0,0 +1,118 @@
+import json
+
+
+x = {
+  "autore": "Lev Nikolaevic Tolstoj",
+  "titolo": "Guerra e pace" ,
+  "genere": "romanzo storico"
+}
+
+y = json.dumps(x)
+
+print(y)
+
+
+
+a = {
+  "autore": "Franz Kafka",
+  "titolo": "Il processo" ,
+  "genere": "romanzo"
+}
+
+y = json.dumps(a)
+
+print(y)
+
+
+b = {
+  "autore": " Italo Calvino",
+  "titolo": "Il barone rampante" ,
+  "genere": "romanzo"
+}
+
+y = json.dumps(b)
+
+print(y)
+
+
+
+c = {
+  "autore": "Khaled Hosseini",
+  "titolo": "Il cacciatore di aquiloni" ,
+  "genere": "romanzo"
+}
+
+y = json.dumps(c)
+
+print(y)
+
+
+
+d = {
+  "autore": "Hanya Yanagihara",
+  "titolo": "Una vita come tante" ,
+  "genere": "romanzo"
+}
+
+y = json.dumps(d)
+
+print(y)
+
+
+
+e = {
+  "autore": "Bill Bryson",
+  "titolo": "Breve storia di quasi tutto" ,
+  "genere": "divulgazione scientifica"
+}
+
+y = json.dumps(e)
+
+print(y)
+
+
+f = {
+  "autore": "Enrico Galiano",
+  "titolo": "Eppure cadiamo felici" ,
+  "genere": "narrativa"
+}
+
+y = json.dumps(f)
+
+print(y)
+
+
+g = {
+  "autore": "Jane Austen",
+  "titolo": "Orgoglio e pregiudizio" ,
+  "genere": "romanzo"
+}
+
+y = json.dumps(g)
+
+print(y)
+
+
+
+h = {
+  "autore": "Stephen King",
+  "titolo": "Shining" ,
+  "genere": "romanzo horror"
+}
+
+y = json.dumps(h)
+
+print(y)
+
+
+
+i = {
+  "autore": "Theodore Sturgeon",
+  "titolo": "Cristalli sognanti" ,
+  "genere": "narrativa fantasy"
+}
+
+y = json.dumps(i)
+
+print(y)
+
