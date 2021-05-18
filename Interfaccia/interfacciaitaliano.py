e9d87587acdfe11c30c991c64acdf3b3c2a0afe5
VittoriaArcione <73583881+VittoriaArcione@users.noreply.github.com>
2021-05-18 13:35:56 +0200

    .

--- Interfaccia/interfacciaitaliano.py
+++ Interfaccia/interfacciaitaliano.py
@@ -1,4 +1,3 @@
-import turtle
 from tkinter import*
 from tkinter import messagebox
 def istruzioni():
@@ -6,8 +5,9 @@ def istruzioni():
 root=Tk()
 root.geometry('600x600')
 root.title("GENERATORE DI HAIKU AUTOMATICO")
-lbl=Label(root,text="Benvenuto nel generatore automatico di Haiku!", font=(20))
+root.configure(background="orange")
+lbl=Label(root,text="Benvenuto nel generatore automatico di Haiku!", fg='black', font=("times new roman", 20), background="yellow")
 lbl.pack()
-b=Button(root, text="Cliccami per le istruzioni d'uso", command=istruzioni)
+b=Button(root, text="Cliccami per le istruzioni d'uso", fg= 'black', command=istruzioni, font=("georgia", 15), background="yellow")
 b.pack()
-
+root.mainloop()
