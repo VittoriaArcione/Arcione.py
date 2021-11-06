b53edf3b046043e1a84c541d6add38f357d563da
VittoriaArcione <73583881+VittoriaArcione@users.noreply.github.com>
2021-11-06 17:45:45 +0100

    .

--- coniche/rette.py
+++ coniche/rette.py
@@ -2,10 +2,19 @@ class retta:
 
     def __init__(self,a,b,c):
     
-        self.__a = a  
-        self.__b = b
-        self.__c = c
+        self.__a = float(a)
+        self.__b = float(b)
+        self.__c = float(c)
         
+    def getA(self):
+        return self.__a
+
+    def getB(self):
+        return self.__b
+    
+    def getC(self):
+        return self.__c
+      
     def equazione_imp(self):
         if float(self.__c) == 0:
             return f'{self.__a}x + {self.__b} = 0'
@@ -34,8 +43,36 @@ class retta:
         else:
             return f'k = -{self.__a}/{self.__b} '
 
-a_b_c = retta (input("a: "), input("b: "), input("c: "))
+   
+    def fascio(self,a1,b1,c1): 
 
+        if float (a1) == float(self.__a):
+            return f'il fascio è improprio e la sua equazione è y= {self.__a}x + q '
+        else:
+            return f'il fascio è proprio e la sua equazione è {self.__a}x + {a1}kx + {self.__b}y + {b1}ky + {self.__c} +  {c1}k = 0 '
+    def intersezione(self, a1, b1, c1):
+        if -self.__a/self.__b == -a1/b1:
+            return None
+        
+        if -self.__a/self.__b == -a1/b1 and -self.__c/self.__b == -c1/b1:
+            return self
+        
+        x = ((self.__c/self.__b)-(c1/b1))/((-self.__a/self.__b)+(a1/b1))
+        y = (-self.__a/self.__b)*x+(-self.__c/self.__b)
+        print("le coordinate dei punti sono: ")
+        return round(x, 2), round(y, 2) 
+        
+
+a_b_c = retta (input("a: "), input("b: "), input("c: "))
+a1 = float(input("a1: "))
+b1 = float(input("b1: "))
+c1 = float(input("c1: "))
+
+print(a_b_c.getA())
+print(a_b_c.getB())
+print(a_b_c.getC())
 print(a_b_c.equazione_imp())
 print(a_b_c.equazione_esp())
 print(a_b_c.coefficiente())
+print(a_b_c.fascio(a1, b1, c1))
+print(a_b_c.intersezione(a1, b1, c1))
