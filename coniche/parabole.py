bd8aec4dbcaf94bdd2dd5726ac8dcc6a11a4155b
VittoriaArcione <73583881+VittoriaArcione@users.noreply.github.com>
2021-11-20 15:42:48 +0100

    Update parabole.py

--- coniche/parabole.py
+++ coniche/parabole.py
@@ -15,13 +15,13 @@ class parabola:
         return self.__c
 
     def fuoco(self):
-    #parabola con asse parallelo all'asse y
+    # parabola con asse parallelo all'asse y
        x =  (-self.__b)/(self.__a)*2
        y = (1) -pow((self.__b),2)+ (self.__a)*(self.__c)*4/4*(self.__a)
        return round(x, 2), round(y, 2)
 
     def fuoco1(self):
-    #parabola con asse parallelo all'asse x
+    # parabola con asse parallelo all'asse x
        x = (1) -pow((self.__b),2)+ (self.__a)*(self.__c)*4/4*(self.__a)
        y = (-self.__b)/(self.__a)*2
        return round(x, 2), round(y, 2)
@@ -29,14 +29,28 @@ class parabola:
     def direttrice(self): 
     # parabola con asse parallelo all'asse y
       y = (-1) -(self.__b)**2 + 4*(self.__a)*(self.__c)/4*(self.__a)
-      print("l'equazione della retta direttrice di una parabola con asse parallelo all'asse y è:")
-      return (y)
+      return f' l equazione della retta direttrice di una parabola con asse parallelo all asse y è y=', (y)
 
     def direttrice1(self):
     # parabola con asse prallelo all'asse x
       x = -1 -(self.__b)**2 + 4*(self.__a)*(self.__c)/4*(self.__a)
-      print("l'equazione della retta direttrice di una parabola con asse parallelo all'asse x è:")
-      return (x)
+      return f' l equazione della retta direttrice di una parabola con asse parallelo all asse y è y=', (x)
+
+
+    def asse(self):
+    # parabola con asse parallelo all'asse delle y 
+      y = -(self.__b)/(self.__a)*2
+      return f' l equazione dell asse parallelo allasse y è y=', (y)
+
+    def asse1(self):
+    # parabola con asse parallelo all'asse delle x
+      y = -(self.__b)/(self.__a)*2
+      return "l'equazione dell'asse di una parabola con asse parallelo all'asse y è y =", (y)
+    
+
+
+
+
 a_b_c = parabola(input("a: "), input("b: "), input("c: "))
 
 print(a_b_c.GetA())
@@ -46,3 +60,5 @@ print(a_b_c.fuoco())
 print(a_b_c.fuoco1())
 print(a_b_c.direttrice())
 print(a_b_c.direttrice1())
+print(a_b_c.asse())
+print(a_b_c.asse1())
