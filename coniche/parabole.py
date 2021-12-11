c1a9f9960a71c116d41fa6272970e3749ce4aa38
VittoriaArcione <73583881+VittoriaArcione@users.noreply.github.com>
2021-12-11 14:57:41 +0100

    Update parabole.py

--- coniche/parabole.py
+++ coniche/parabole.py
@@ -16,36 +16,39 @@ class parabola:
 
     def fuoco(self):
     # parabola con asse parallelo all'asse y
+       
        x =  (-self.__b)/(self.__a)*2
        y = (1) -pow((self.__b),2)+ (self.__a)*(self.__c)*4/4*(self.__a)
+       print("le coordinate del fuoco con asse parallelo all'asse y sono:")
        return round(x, 2), round(y, 2)
 
     def fuoco1(self):
     # parabola con asse parallelo all'asse x
        x = (1) -pow((self.__b),2)+ (self.__a)*(self.__c)*4/4*(self.__a)
        y = (-self.__b)/(self.__a)*2
+       print("le coordinate del fuoco con asse parallelo all'asse x sono:")
        return round(x, 2), round(y, 2)
 
     def direttrice(self): 
     # parabola con asse parallelo all'asse y
-      y = (-1) -(self.__b)**2 + 4*(self.__a)*(self.__c)/4*(self.__a)
+      y = -1 -(self.__b)**2 + 4*(self.__a)*(self.__c)/4*(self.__a)
       return f' l equazione della retta direttrice di una parabola con asse parallelo all asse y è y=', (y)
 
     def direttrice1(self):
     # parabola con asse prallelo all'asse x
       x = -1 -(self.__b)**2 + 4*(self.__a)*(self.__c)/4*(self.__a)
-      return f' l equazione della retta direttrice di una parabola con asse parallelo all asse y è y=', (x)
-
+      return f' l equazione della retta direttrice di una parabola con asse parallelo all asse x è x=', (x)
 
     def asse(self):
     # parabola con asse parallelo all'asse delle y 
       y = -(self.__b)/(self.__a)*2
-      return f' l equazione dell asse parallelo allasse y è y=', (y)
+      return f' l equazione dell asse parallelo allasse y è y=:', (y)
+      
 
     def asse1(self):
     # parabola con asse parallelo all'asse delle x
       y = -(self.__b)/(self.__a)*2
-      return "l'equazione dell'asse di una parabola con asse parallelo all'asse y è y =", (y)
+      return f'l equazione dell asse di una parabola con asse parallelo all asse x è x=', (y)
     
 
 
