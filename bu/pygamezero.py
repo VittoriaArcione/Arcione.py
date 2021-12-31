d6442ed74de80f920581cdf4b8d3276e84375c46
VittoriaArcione <73583881+VittoriaArcione@users.noreply.github.com>
2021-12-31 17:17:25 +0100

    .

--- bu/pygamezero.py
+++ bu/pygamezero.py
@@ -1,5 +1,5 @@
 import pgzrun
-gennaro=Actor('gennaro',(55, 180))
+gennaro=Actor('gennaro',(400, 300))
 WIDTH=800
 HEIGHT=600
 
@@ -22,13 +22,13 @@ def update():
 def on_mouse_down(pos):
    if gennaro.collidepoint(pos):
        set_gennaro_hit()
-       print("EEEEEEEEEEK")
+       print("SWUSH")
    else:
        print("eheh")
 
 def set_gennaro_hit():
    gennaro.image= "megacalcio"
-   clock.schedule_unique(set_gennaro_normal, 0.5)
+   clock.schedule_unique(set_gennaro_normal, 0.2)
    #sound.play()
 
 def set_gennaro_normal():
--- coniche/parabole.py
+++ coniche/parabole.py
@@ -5,16 +5,17 @@ class parabola:
             self.__a = int(p1)  
             self.__b = int(p2)
             self.__c = int(p3)
-            
-
+  
         elif(tipo == "fuocoDiret"):
             self.__p1 = int(p1)
-            self.__p2 = int(p2)
+            self.__p2 = int(p2)  #xf
             self.__p3 = int(p3)
             
             self.__a = 1/(2*self.__p2 - 2*self.__p3)
             self.__b = -2*self.__a * self.__p1
             self.__c = (4*self.__p2*self.__a + self.__b*self.__b - 1)/(4*self.__a)
+
+        
         
     def GetA(self):
         return self.__a
@@ -27,14 +28,15 @@ class parabola:
 
     def fuoco(self, asse_simmetria = "x"):
         if (asse_simmetria == "x"):
-           x =  (-self.__b)/(self.__a)*2
-           y = (1) -pow((self.__b),2)+ (self.__a)*(self.__c)*4/4*(self.__a)
+           x = (1) -pow((self.__b),2)+ (self.__a)*(self.__c)*4/4*(self.__a)
+           y =  (-self.__b)/(self.__a)*2
+          
            print("le coordinate del fuoco con asse parallelo all'asse x sono:")
            return round(x, 2), round(y, 2)
 
         elif (asse_simmetria == "y"):
-            x = (1) -pow((self.__b),2)+ (self.__a)*(self.__c)*4/4*(self.__a)
-            y = (-self.__b)/(self.__a)*2 
+            x = (-self.__b)/(self.__a)*2  
+            y = (1) -pow((self.__b),2)+ (self.__a)*(self.__c)*4/4*(self.__a)
             print("le coordinate del fuoco con asse parallelo all'asse x sono:")
             return round(x, 2), round(y, 2)
 
@@ -60,14 +62,12 @@ class parabola:
           return f'l equazione dell asse di una parabola con asse parallelo all asse y è x = {x}'
     
 
+  
 
 
+a_b_c = parabola(input('tipo: '),input('val 1: '), input('val 2: '), input('val 3: '))
 
-a_b_c = parabola(input('tipo: '),input('a: '), input('b: '), input('c: '))
 
-print(a_b_c.GetA())
-print(a_b_c.GetB())
-print(a_b_c.GetC())
-print(a_b_c.fuoco(input("asse:")))
-print(a_b_c.direttrice(input("asse:")))
-print(a_b_c.asse(input("asse:")))
+print(a_b_c.fuoco(input("inserire l'asse parallelo a quello della parabola per ottenere le coordinate del fuoco:")))
+print(a_b_c.direttrice(input("inserire l'asse parallelo a quello della parabola per ottenere l'equazione della retta direttrice: ")))
+print(a_b_c.asse(input("inserire l'asse parallelo a quello della parabola per ottenere l'equazione dell'asse:")))
